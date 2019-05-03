4eb56027f1d7d0dbe7fbb578d4868cb7126df45e
Youngsoo, Na <pullinear@naver.com>
2019-05-04 00:38:55 +0900

    simple design for nfa and dfa class

--- NFAtoDFA.py
+++ NFAtoDFA.py
@@ -13,26 +13,34 @@
 #  string = [a, b] + epsilon
 #  state = ["q0", "q1" , "q2" , "q3"]
 
-class DFA:
+class NFA:
     states = []
     start_state = 0
     end_states = []
     strings = []
-    delta = []
+    delta = {}
 
-    def __init__(self, states:list, start_state: int, end_states: list, strings: list, delta: list):
+    def __init__(self, states: list, start_state: int, end_states: list, strings: list, delta: list):
         self.states = states
         self.start_state = start_state
         self.end_states = end_states
         self.strings = strings
         self.delta = delta
-    
+
+    def delta_func(self, state, char) ->list:
+        if state in self.delta.keys():
+            if char in self.delta[state].keys():
+                return self.delta[state][char]
+
+        return "None"
+
+class DFA(NFA):
+    def __init__(self, states:list, start_state: int, end_states: list, strings: list, delta: list):
+        super().__init__(states, start_state, end_states, strings, delta)
 
 
-def DFA(states: list, start_state: int, end_states: list, string: list, delta: list):
-    dfa = {}
 
 
-def NFAtoDFA():
+def NFAtoDFA(nfa, dfa):
     pass
 
